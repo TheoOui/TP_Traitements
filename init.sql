0fb35f39a7da6f038378f2678e2eadd1680842b7
Clément Guiton <clement.guiton.dev@gmail.com>
2024-06-27 16:05:48 +0200

    insert event

--- init.sql
+++ init.sql
@@ -2,11 +2,14 @@ drop database if exists ticketdb;
 create database ticketdb;
 
 create table ticketdb.events (
-    event_id int primary key,
+    event_id int primary key AUTO_INCREMENT,
     available_tickets int
 );
 
+insert into ticketdb.events (available_tickets) values (100);
+
 create table ticketdb.reservations (
-    reversvation_id int primary key,
+    reservations_id int primary key AUTO_INCREMENT,
+    user_id int,
     event_id int references ticketdb.events(event_id)
 );
